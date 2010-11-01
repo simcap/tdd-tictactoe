7f1aa83b5cc3ed73e7fe5ef54f4c9a72cb94fd91
simcap <caplette@hotmail.co.uk>
2010-11-01 14:35:43 +0100

    refactoring - detection methods return now an int

--- src/main/java/fr/xebia/training/tdd/tictactoe/Game.java
+++ src/main/java/fr/xebia/training/tdd/tictactoe/Game.java
@@ -4,25 +4,31 @@ public class Game {
 	
 	private int[][] grid = new int[3][3];
 
-	public boolean put(int i, int j, int player) {
+	public int put(int i, int j, int player) {
 		grid[i][j] = player;
-		return hasColumn(1) || hasColumn(2) ||
-			   hasRow(1) || hasRow(2) ||
-			   hasDiagonal(1) || hasDiagonal(2);                                          
+		if(hasColumn(player) == 1 || hasRow(player) == 1 || hasDiagonal(player) == 1){
+			return 1;
+		} else return 0;
 	}
 
-	private boolean hasColumn(int player) {
-		return (grid[0][0] == player && grid[0][1] == player && grid[0][2] == player)
-		 	|| (grid[1][0] == player  && grid[1][1] == player && grid[1][2] == player) 
-            || (grid[2][0] == player  && grid[2][1] == player && grid[2][2] == player);
+	private int hasColumn(int player) {
+		if((grid[0][0] == player && grid[0][1] == player && grid[0][2] == player)
+				|| (grid[1][0] == player  && grid[1][1] == player && grid[1][2] == player) 
+				|| (grid[2][0] == player  && grid[2][1] == player && grid[2][2] == player)){
+			return 1;
+		} else return 0;
 	}                                                   
-	private boolean hasRow(int player) {
-		return (grid[0][0] == player && grid[1][0] == player && grid[2][0] == player)
-		|| (grid[0][1] == player  && grid[1][1] == player && grid[2][1] == player) 
-		|| (grid[0][2] == player  && grid[1][2] == player && grid[2][2] == player);
+	private int hasRow(int player) {
+		if ((grid[0][0] == player && grid[1][0] == player && grid[2][0] == player)
+				|| (grid[0][1] == player  && grid[1][1] == player && grid[2][1] == player) 
+				|| (grid[0][2] == player  && grid[1][2] == player && grid[2][2] == player)){
+			return 1;
+		} return 0;
 	}                                                   
-	private boolean hasDiagonal(int player) {
-		return (grid[0][0] == player && grid[1][1] == player && grid[2][2] == player)
-		|| (grid[0][2] == player  && grid[1][1] == player && grid[2][0] == player); 
+	private int hasDiagonal(int player) {
+		if((grid[0][0] == player && grid[1][1] == player && grid[2][2] == player)
+				|| (grid[0][2] == player  && grid[1][1] == player && grid[2][0] == player)){
+			return 1;
+		} else return 0;
 	}
 }
